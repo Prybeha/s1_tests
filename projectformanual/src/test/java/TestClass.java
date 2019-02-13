93a0620d7ee7dff808e6e69f6b587cc9b67ea7c8
Volodymyr Prybeha <vprybeha@dynamo-kiev.local>
2019-02-13 17:46:17 +0200

    reg + inv
    v.5

--- projectformanual/src/test/java/TestClass.java
+++ projectformanual/src/test/java/TestClass.java
@@ -26,25 +26,7 @@ public class TestClass extends SetupClass {
         // Checking all fields in My Account for no invested users
 
         PagesURL url = new PagesURL();
-        url.LoginPage();
-        Login l = new Login();
-        l.LoginTest(IndividualUS);
+        url.FundsPage();
 
-        url.DealPage();
-        SetupClass.GetDriver().findElement(By.xpath("//a[@href='https://secure-seriesone.dynamo-ny.com/deal/pdp-qa']")).click();
-        //SetupClass.GetDriverWait().until(ExpectedConditions.titleIs("Kinesis QA | seriesOne"));
-        Thread.sleep(600);
-
-        SetupClass.GetDriver().findElement(By.xpath("//a[text()='Invest']")).click();
-        if (field.ExistElementOnThePage("//div[@class='bootbox modal fade modal-edit-profile popup-agreement in']",2)){
-            if (!SetupClass.GetDriver().findElement(By.xpath("//input[@name='firstAgreement']")).isSelected()) {
-                SetupClass.GetDriver().findElement(By.xpath("/html/body/div[4]/div/div/div[2]/div/div/div[1]/div/label/div/div/label[2]")).click();
-            }
-            if (!SetupClass.GetDriver().findElement(By.xpath("//input[@name='secondAgreement']")).isSelected()) {
-                SetupClass.GetDriver().findElement(By.xpath("/html/body/div[4]/div/div/div[2]/div/div/div[2]/div/label/div/div/label[2]")).click();
-            }
-            Thread.sleep(200);
-            SetupClass.GetDriver().findElement(By.xpath("//button[text()='Confirm']")).click();
-        }
     }
 }
